7ba098a0cd3129e4f3f4789291d4bf603d49a79e
violethsu <glairmorning@gmail.com>
2018-10-23 21:46:49 +0800

    Initial Commit

---
+++ MaPle/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstViewController.swift
+//  MaPle
+//
+//  Created by Violet on 2018/10/23.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MaPle/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  MaPle
+//
+//  Created by Violet on 2018/10/23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
