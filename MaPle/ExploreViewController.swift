caeb3fce028ffd97f2d571f51c0837ea21314269
violethsu <glairmorning@gmail.com>
2018-10-23 23:26:42 +0800

    Vio: (1)完成tabBar (2)APPicon設置

--- MaPle/ExploreViewController.swift
+++ MaPle/ExploreViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class ExploreViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- MaPle/MyPageController.swift
+++ MaPle/MyPageController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class MyPageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
