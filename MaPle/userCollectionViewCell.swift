e810ed9f39473e9a1ca1be804d67f4afd287fd87
張正宇 <iii00712@gmail.com>
2018-11-29 11:11:20 +0800

    登入註冊功能完成

---
+++ MaPle/userCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  userCollectionViewCell.swift
+//  MaPle
+//
+//  Created by Bron on 2018/11/26.
+//
+
+import UIKit
+
+class userCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleUITextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
+}
