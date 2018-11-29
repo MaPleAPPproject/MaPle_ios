dd804879c0d5611d6e59fcac4ea2f00bb2842756
violethsu <glairmorning@gmail.com>
2018-11-29 10:34:53 +0800

    Violet:Explore頁面version1(memberid為假資料)

---
+++ MaPle/ResultMapCollectionReusableView.swift
@@ -0,0 +1,14 @@
+//
+//  ResultMapCollectionReusableView.swift
+//  MaPle
+//
+//  Created by Violet on 2018/11/28.
+//
+
+import UIKit
+import MapKit
+
+class ResultMapCollectionReusableView: UICollectionReusableView {
+        
+    @IBOutlet weak var resultmapView: MKMapView!
+}
